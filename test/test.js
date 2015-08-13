7a2b96b31dbbccdfb400dd0b950d3a18304219e8
andyjansson <andyjansson@users.noreply.github.com>
2015-08-13 09:13:40 +0200

    Add mocha

--- test/test.js
+++ test/test.js
@@ -4,8 +4,16 @@ var woff2 = require('../');
 var assert = require('assert');
 var expected = require('./expected.json');
 
-fs.readFile(path.join(__dirname, 'pathFont.woff2'), function (err, contents) {
-	woff2(contents).then(function (results) {
-		assert.deepEqual(results, expected);
+describe('woff2-parser', function () {
+	it('can parse .woff2 fonts', function (done) {
+		fs.readFile(path.join(__dirname, 'pathFont.woff2'), function (err, contents) {
+			if (err) throw err;
+			woff2(contents).then(function (results) {
+				assert.deepEqual(results, expected);
+				done();
+			}, function () {
+				assert.fail("Parsing font failed");
+			});
+		});
 	});
 });
