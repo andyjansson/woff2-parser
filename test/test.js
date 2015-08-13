94b69e07983597f0c714c56f22511351d5e36c76
andyjansson <andyjansson@users.noreply.github.com>
2015-08-13 08:57:01 +0200

    Add test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-/*
 var path = require('path');
 var fs = require('fs');
 var woff2 = require('../');
+var assert = require('assert');
+var expected = require('./expected.json');
 
 fs.readFile(path.join(__dirname, 'pathFont.woff2'), function (err, contents) {
 	woff2(contents).then(function (results) {
-		console.log(results);
+		assert.deepEqual(results, expected);
 	});
 });
-*/
